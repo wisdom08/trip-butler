9c12a295e9214f725499cb77f9722e0b6caa094e
wisdom08 <wisdom08@kakao.com>
2022-08-26 19:21:30 +0900

    feat: 프로젝트 셋업

---
+++ src/main/java/com/trip/butler/TripButlerApplication.java
@@ -0,0 +1,13 @@
+package com.trip.butler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripButlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripButlerApplication.class, args);
+    }
+
+}
