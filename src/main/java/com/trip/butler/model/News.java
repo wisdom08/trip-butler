1fc22a262b83cb0790d4b6169439835d24d7984b
jeanniejang <jhjang.sdev@gmail.com>
2022-08-29 17:07:18 +0900

    feat: #5 News entity 기고일자 추가 -Timestamped 생성일자 기능 삭제 -News entity에 기고일자 필드로 추가

--- src/main/java/com/trip/butler/model/News.java
+++ src/main/java/com/trip/butler/model/News.java
@@ -4,16 +4,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Getter
-public class News extends Timestamped{
+public class News{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name= "news_id")
     private Long id;
-
+    private LocalDateTime date;
     private String press;
     private String author;
     private String title;
