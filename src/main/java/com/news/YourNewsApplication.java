4ad2b24fb8457c962fd3bd5b6144ae4df5b9b12c
jeanniejang <jhjang.sdev@gmail.com>
2022-09-14 18:22:40 +0900

    feat: #29 키워드 검색 기능 구현 -elasticsearch로 바뀜에 따라 News document 생성 -query를 받을 SearchRequestDto 생성 -받은 query를 elasticsearch에서 처리해줄 SearchUtil 생성 -검색 기능 구현을 위한 service와 controller 생성

--- src/main/java/com/news/YourNewsApplication.java
+++ src/main/java/com/news/YourNewsApplication.java
@@ -2,8 +2,9 @@ package com.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
 public class YourNewsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/news/service/NewsService.java
+++ src/main/java/com/news/service/NewsService.java
@@ -1,26 +1,58 @@
 package com.news.service;
 
-import com.news.dto.NewsDto;
-import com.news.model.News;
-import com.news.repository.NewsRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.news.document.News;
+import com.news.dto.SearchRequestDto;
+import com.news.dto.util.SearchUtil;
+import com.news.helper.Indices;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.search.SearchHit;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NewsService {
-    private final NewsRepository newsRepository;
-    public List<NewsDto> searchNews(String keyword) {
-        List<News> newsList = newsRepository.findAllByKeywordContainingOrderByDateDesc(keyword);
-        List <NewsDto> newsDtoList = new ArrayList<>();
-        for (News news : newsList){
-            newsDtoList.add(NewsDto.builder()
-                    .news(news)
-                    .build());
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private final RestHighLevelClient client;
+
+    public List<News> search(SearchRequestDto dto) {
+        final SearchRequest request = SearchUtil.buildSearchRequest(Indices.NEWS_INDEX, dto);
+
+        return searchInternal(request);
+    }
+
+
+    public List<News> searchInternal(SearchRequest request) {
+        if (request == null) {
+            log.error("Failed to build search request");
+            return Collections.emptyList();
         }
-        return newsDtoList;
+
+        try {
+            final SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+            final SearchHit[] searchHits = response.getHits().getHits();
+            final List<News> newsList = new ArrayList<>(searchHits.length);
+            for (SearchHit hit : searchHits) {
+                newsList.add(
+                        MAPPER.readValue(hit.getSourceAsString(), News.class)
+                );
+            }
+
+            return newsList;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return Collections.emptyList();
+        }
     }
 }
