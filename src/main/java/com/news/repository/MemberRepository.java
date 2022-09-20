f30d6113e55485e6c5eac9cc94c044124225a38d
Jungmin-Noh <lumill925@gmail.com>
2022-09-20 16:44:25 +0900

    Revert "feat: #44 Member 관련 entity, repository, dto 생성"
    This reverts commit dfeef1796e956431d46bac0df7c34b73f295f240.

--- src/main/java/com/news/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.news.repository;
-
-import com.news.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
