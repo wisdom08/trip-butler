2f2227bc0e5cfde8220e8714d2c8e8e9ac94364e
wisdom08 <wisdom08@kakao.com>
2022-09-23 14:35:58 +0900

    fix: #42 게시글 작성 api 추가

---
+++ src/main/java/com/news/entity/Post.java
@@ -0,0 +1,27 @@
+package com.news.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String newsId;
+    private String contents;
+
+    protected Post() {}
+
+    private Post(String newsId, String contents) {
+        this.newsId = newsId;
+        this.contents = contents;
+    }
+
+    public static Post createPost(String newsId, String contents) {
+        return new Post(newsId, contents);
+    }
+}
