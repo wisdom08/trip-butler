df86b73298205be7ffc56256e16063be88411947
Jungmin-Noh <lumill925@gmail.com>
2022-09-26 16:23:43 +0900

    fix: #44 html script 각각 분리

---
+++ src/main/resources/static/login.js
@@ -0,0 +1,25 @@
+function click_login() {
+    // 로그인용 data 준비
+    let payload = {email: $('#email').val(), password: $('#password').val()};
+    // 로그인 통신
+    $.ajax({
+        type: 'POST',
+        url: '/api/user/login',
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(payload),
+        success: function (result) {
+            console.log(result)
+            // 성공
+            sessionStorage.setItem("accessToken", result.accessToken)
+            sessionStorage.setItem("email", result.email)
+            sessionStorage.setItem("nickname", result.nickname)
+            sessionStorage.setItem("imageUrl", result.imageUrl)
+        }, complete: function () {
+            // 성공 후 로직
+            location.href = "/";
+        }
+    }).fail(function (error) {
+        alert('로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.');
+    });
+}
