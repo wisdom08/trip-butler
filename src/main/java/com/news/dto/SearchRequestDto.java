e748140191c56f679a4df43c2f31d9550fc9c431
jeanniejang <jhjang.sdev@gmail.com>
2022-09-29 17:25:53 +0900

    feat: #39 서버 측 filter 검색 기능 추가 -boolquery의 filter를 사용해 기간, 분야, 신문사별로 검색할 수 있는 기능 추가

--- src/main/java/com/news/dto/SearchRequestDto.java
+++ src/main/java/com/news/dto/SearchRequestDto.java
@@ -1,13 +1,12 @@
 package com.news.dto;
 
 import lombok.Getter;
-import lombok.Setter;
-
-import java.util.List;
 
 @Getter
-@Setter
 public class SearchRequestDto {
-    private List<String> fields;
+    private String[] section;
+    private String[] press;
     private String searchTerm;
+    private String startDate;
+    private String endDate;
 }
