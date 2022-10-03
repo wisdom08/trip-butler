79005d6b6be5f1cb48ae7fdd7647ca9482e10ac9
wisdom08 <wisdom08@kakao.com>
2022-10-03 09:19:34 +0900

    feat: #60 의견 확인용 모달 추가

--- src/main/java/com/news/controller/ViewPageController.java
+++ src/main/java/com/news/controller/ViewPageController.java
@@ -1,12 +1,21 @@
 package com.news.controller;
 
+import com.news.service.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewPageController {
+
+    private final PostService postService;
+    public ViewPageController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/")
-    public String getHome() {
+    public String getHome(Model model) {
+        model.addAttribute("posts", postService.getMyPosts());
         return "index";
     }
 
